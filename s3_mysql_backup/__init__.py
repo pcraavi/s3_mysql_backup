a5b163f08d7d5b39c5c9c44b371aefb1bd1401f9
marc condon <marc@fogtest.com>
2016-09-03 17:04:49 -0700

    fixed missing boto import

--- s3_mysql_backup/__init__.py
+++ s3_mysql_backup/__init__.py
@@ -6,6 +6,8 @@ import subprocess
 from datetime import datetime as dt
 from datetime import timedelta as td
 
+import boto
+
 YMD_FORMAT = '%Y-%m-%d'
 TIMESTAMP_FORMAT = '%Y-%m-%d-%H-%M-%S'
 DIR_CREATE_TIME_FORMAT = '%a %b %d %H:%M:%S %Y'
